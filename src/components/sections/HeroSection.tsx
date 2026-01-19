d7564a5b2ffee82fe30e248c21a393d1e0e033a4
Egor Kharitonov <ega.khar@gmail.com>
2026-01-18 22:44:37 -0500

    fix: techx video vercel blob swap

--- src/components/sections/HeroSection.tsx
+++ src/components/sections/HeroSection.tsx
@@ -1,16 +1,10 @@
 import RegistrationButtons from '../elements/RegistrationButtons'
+import techxVideo from '../../assets/techx_video.webm'
 
 export default function HeroSection() {
   return (
     <div className={'relative w-full h-[35rem] md:h-[38rem] bg-stone-200 overflow-hidden'}>
-      <video
-        src="https://2gr9zmtfdk383c1z.public.blob.vercel-storage.com/techx_video.webm"
-        className={'w-full h-full object-cover'}
-        autoPlay
-        muted
-        loop
-        playsInline
-      />
+      <video src={techxVideo} className={'w-full h-full object-cover'} autoPlay muted loop playsInline />
       <div className={'absolute top-0 opacity-65 w-full h-full bg-black z-1'} />
       <div className={'absolute top-10 md:top-45 flex flex-col justify-center items-center z-2 gap-4 w-full pl-4 pr-4'}>
         <h1 className={'text-white text-4xl md:text-5xl xl:text-6xl text-center font-semibold'}>TechX Florida 2025 AI Conference</h1>
